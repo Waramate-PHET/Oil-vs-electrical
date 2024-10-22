a4e1dd5209a5972923309656c3813a42111cfb21
Waramate-PHET <waramet1107@gmail.com>
2024-10-22 20:44:26 +0700

    update App.py

--- App.py
+++ App.py
@@ -1,4 +1,8 @@
 import streamlit as st
 
 st.write("ใส่ code Pythone ในนี้")
-st.button("OK")
+st.title("Button Example")
+if st.button("Click Me!"):
+    st.write("You clicked the button!")
+else:
+    st.write("Please click the button.")
