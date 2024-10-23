361124c52e222f9ceb26fbf9eadbbbd44a91c8f0
WIKHANETqp <wikhanett99@gmail.com>
2024-10-23 17:35:13 +0700

    Add App3.py

---
+++ App3.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+st.write("ใส่ code Pythone ในนี้")
+st.title("Button Example")
+if st.button("Click Me!"):
+    st.write("You clicked the button!")
+else:
+    st.write("Please click the button.")
+    st.write("yoyoyoyoy.")
+    
